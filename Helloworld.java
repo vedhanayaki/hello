8e3f45176ca527d37b844cd7d7c51ab85a4ee6db
vedhanayaki <vedhanayaki1996@gmail.com>
2017-07-19 01:55:11 -0700

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,12 @@
+package player;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s1="Hello";
+String s2="World";
+System.out.println(s1+"\t"+s2);
+	}
+
+}
